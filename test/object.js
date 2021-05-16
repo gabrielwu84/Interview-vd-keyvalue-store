fc85f4b3ae7bcef8d0c648205590c8257a9f165c
Gabriel Wu <gabrielwu84@gmail.com>
2021-05-16 22:58:20 +0800

    extracted testing parameter into a variable in preparation for more complex scenerio

--- test/object.js
+++ test/object.js
@@ -9,31 +9,32 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 describe('Objects', () => {
+    let testObj1 = { mykey: "value1" };
     describe('/POST /object', () => {
         it('it should POST a key-value object and return the object with a timestamp', (done) => {
             chai.request(server)
                 .post('/object')
-                .send( { mykey: "value1" } )
+                .send( testObj1 )
                 .end((err, res) => {
                     console.log(res.text)
                     res.should.have.status(200);
                     res.body.should.be.a('object');
-                    res.body.should.have.property('key').eql( 'mykey' );
-                    res.body.should.have.property('value').eql( 'value1' );
+                    res.body.should.have.property('key').eql( Object.keys(testObj1)[0] );
+                    res.body.should.have.property('value').eql( Object.values(testObj1)[0] );
                     res.body.should.have.property('timestamp');
                     done();
                 });
         });
     });
-    describe('/GET /object/', () => {
-        it('it should GET value of key mykey', (done) => {
+    describe('/GET /object/' + Object.keys(testObj1)[0], () => {
+        it('it should GET value of object by given key', (done) => {
             chai.request(server)
-                .get('/object/mykey')
+                .get('/object/'+ Object.keys(testObj1)[0])
                 .end((err,res)=>{
                     console.log(res.text)
                     res.should.have.status(200);
                     res.body.should.be.a('object');
-                    res.body.should.have.property('value').eql( 'value1' );
+                    res.body.should.have.property('value').eql( Object.values(testObj1)[0] );
                     done()
                 })
         });
