d4e2dbf02c3ff4386dce6feb992db1bb1e189c78
Gabriel Wu <gabrielwu84@gmail.com>
2021-05-16 22:27:08 +0800

    added connection to db and bodyParser

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 let express = require('express');
 let app = express();
 let port = 3000;
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+
+// db options
+let options = {
+    useNewUrlParser: true ,
+    useUnifiedTopology: true
+};
+// db connection
+mongoose.connect("mongodb://localhost:27017/object", options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json'}));
 
 app.get("/", (req, res) => res.json({message: "VD key-value store"}));
 app.listen(port);
